7d7f686a69f3ff1eb33f18d946a428e35f060162
xice09 <liangxiao09@163.com>
2018-11-04 20:58:18 +0800

    change th .. to .

--- server/conf.js
+++ server/conf.js
@@ -10,14 +10,14 @@ exports.SECRET_ID = '';
 exports.SECRET_KEY = '';
 exports.USERID = '';
 
-var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../', 'package.json')));
+var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, './', 'package.json')));
 var ua = function() {
     return 'QcloudNodejs/' + pkg.version + ' (' + os.type() + '; ' + os.platform() + '; ' + os.arch() + '; ) ';
 }
 
 exports.USER_AGENT = ua;
-exports.API_YOUTU_SERVER= 'api.youtu.qq.com';
-exports.API_YOUTU_PORT= 80;
+exports.API_YOUTU_SERVER = 'api.youtu.qq.com';
+exports.API_YOUTU_PORT = 80;
 exports.API_DOMAIN = 0;
 
 // 初始化 应用信息 
@@ -26,15 +26,10 @@ exports.setAppInfo = function(appid, secretId, secretKey, userid, domain) {
     module.exports.SECRET_ID = secretId;
     module.exports.SECRET_KEY = secretKey;
     module.exports.USERID = userid;
-    if(domain == 0)
-    {
-       exports.API_YOUTU_SERVER= 'api.youtu.qq.com';
-    }
-    else
-    {
-       exports.API_YOUTU_SERVER= 'youtu.api.qcloud.com';
+    if (domain == 0) {
+        exports.API_YOUTU_SERVER = 'api.youtu.qq.com';
+    } else {
+        exports.API_YOUTU_SERVER = 'youtu.api.qcloud.com';
     }
     exports.API_DOMAIN = domain;
 }
-
-
